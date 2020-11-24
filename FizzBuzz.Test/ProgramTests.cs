aae7c107ab25b373dc2142575a14a70d6168997b
Andreas Zachrisson <Azachris@volvocars.com>
2020-11-24 10:16:01 +0100

    Implements functionality specified in assignment

--- FizzBuzz.Test/ProgramTests.cs
+++ FizzBuzz.Test/ProgramTests.cs
@@ -7,48 +7,117 @@ namespace FizzBuzz.Test
     public class ProgramTests
     {
         [TestMethod]
-        public void If_Input_Is_Evenly_Divisible_By_Three_Should_Return_Fizz()
+        public void If_input_is_evenly_divisible_by_three_Should_return_fizz()
         {
             // Arrange
-            var program = new Program();
             var input = 3;
             var expectedResult = "Fizz";
 
             // Act
-            var result = program.GetFizzBuzz(input);
+            var result = Program.GetOutput(input);
 
             // Assert
             Assert.AreEqual(expectedResult, result);
         }
 
         [TestMethod]
-        public void DividedByOnlyFiveIsBuzz()
+        public void If_input_is_evenly_divisible_by_five_Should_return_buzz()
         {
-            // Arrange 
+            // Arrange
+            var input = 5;
+            var expectedResult = "Buzz";
 
             // Act
+            var result = Program.GetOutput(input);
 
             // Assert
+            Assert.AreEqual(expectedResult, result);
         }
 
         [TestMethod]
-        public void DividedByThreeAndFiveIsFizzBuzz()
+        public void If_input_is_evenly_divisible_by_both_three_and_five_Should_return_fizzbuzz()
         {
-            // Arrange 
+            // Arrange
+            var input = 15;
+            var expectedResult = "FizzBuzz";
 
             // Act
+            var result = Program.GetOutput(input);
 
             // Assert
+            Assert.AreEqual(expectedResult, result);
         }
 
         [TestMethod]
-        public void DividedByFortyTwoIsMeaningOfLife()
+        public void If_input_is_42_Should_return_the_meaning_of_life()
         {
-            // Arrange 
+            // Arrange
+            var input = 42;
+            var expectedResult = "Answer to the Ultimate Question of Life, the Universe, and Everything";
 
             // Act
+            var result = Program.GetOutput(input);
 
             // Assert
+            Assert.AreEqual(expectedResult, result);
         }
+
+        [TestMethod]
+        public void If_input_is_not_divisible_by_three_or_five_and_is_not_fortytwo_Should_return_input_as_string()
+        {
+            // Arrange
+            var input = 1;
+            var expectedResult = input.ToString();
+
+            // Act
+            var result = Program.GetOutput(input);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void If_user_input_is_not_an_int_Should_return_negative_one()
+        {
+            // Arrange
+            var input = "Detta är fel";
+            var expectedResult = -1;
+
+            // Act
+            var result = Program.ValidateInput(input);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [DataTestMethod]
+        [DataRow("0")]
+        [DataRow("666")]
+        public void If_user_input_is_not_between_one_and_threehundred_Should_return_negative_one(string input)
+        {
+            // Arrange
+            var expectedResult = -1;
+
+            // Act
+            var result = Program.ValidateInput(input);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void If_user_input_is_between_one_and_threehundred()
+        {
+            // Arrange
+            var input = "5";
+            var expectedResult = int.Parse(input);
+
+            // Act
+            var result = Program.ValidateInput(input);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
     }
 }
--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace FizzBuzz.Main
 {
@@ -6,14 +7,63 @@ namespace FizzBuzz.Main
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                PrintMenu();
+                PrintResult(GetUserInput());
+            }
         }
 
-        public string GetFizzBuzz(int input)
+        public static void PrintMenu()
         {
+            Console.WriteLine("Enter an integer between 1 and 300");
+        }
+
+        public static int GetUserInput()
+        {
+            return ValidateInput(Console.ReadLine());
+        }
+
+        public static void PrintResult(int input)
+        {
+            if (input == -1)
+            {
+                Console.WriteLine("Input must be an integer between 1 and 300");
+                return;
+            }
+
+            for (int i = 1; i <= input; i++)
+            {
+                Console.WriteLine(GetOutput(i));
+            }
+
+        }
+
+        public static string GetOutput(int input)
+        {
+            if (input == 42)
+                return "Answer to the Ultimate Question of Life, the Universe, and Everything";
+            if (input % 3 == 0 && input % 5 == 0)
+                return "FizzBuzz";
             if (input % 3 == 0)
                 return "Fizz";
-            throw new Exception();
+            if (input % 5 == 0)
+                return "Buzz";
+
+            return input.ToString();
+        }
+
+        public static int ValidateInput(string input)
+        {
+            const int failedValidationCode = -1;
+
+            if (!int.TryParse(input, out var parsedInput)) 
+                return failedValidationCode;
+
+            if (parsedInput < 1 || parsedInput > 300)
+                return failedValidationCode;
+
+            return parsedInput;
         }
     }
 }
