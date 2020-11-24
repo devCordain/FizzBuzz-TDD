2891a7c43a5b122e40d1a4f2768f75244096daca
devCordain <pctkar@gmail.com>
2020-11-24 08:56:43 +0100

    initial commit

---
+++ FizzBuzz.Test/UnitTests.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz.Test
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void DividedByOnlyThreeIsFizz()
+        {
+        }
+        [TestMethod]
+        public void DividedByOnlyFiveIsBuzz()
+        {
+
+        }
+        [TestMethod]
+        public void DividedByThreeAndFiveIsFizzBuzz()
+        {
+
+        }
+        [TestMethod]
+        public void DividedByFourtyTwoIsMeaningOfLife()
+        {
+
+        }
+    }
+}
